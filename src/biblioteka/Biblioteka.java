3a5457989d1345425656c208e82263a543940d2a
VukMan00 <vukman619@gmail.com>
2023-04-18 19:44:49 +0200

    Implementacija metoda klase Biblioteka

---
+++ src/biblioteka/Biblioteka.java
@@ -0,0 +1,38 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs{
+
+	private List<Knjiga> knjige = new ArrayList<>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> VratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		List<Knjiga> rezultati = new ArrayList<Knjiga>();
+		if (naslov!=null)
+		  for (Knjiga k: knjige)
+			if (k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim()))
+				rezultati.add(k);		
+		
+		return rezultati;
+	}
+
+}
