17d743bc2f97437b66e93db8ebc07ee555a38738
VukMan00 <vukman619@gmail.com>
2023-04-18 19:20:44 +0200

    Dodati atributi i get/set metode

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,5 +1,44 @@
 package biblioteka;
 
+import java.util.List;
+
 public class Knjiga {
 
+	private String naslov;
+	private Long isbn;
+	private List<String> autori;
+	private String izdavac;
+	private int izdanje;
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public Long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(Long isbn) {
+		this.isbn = isbn;
+	}
+	public List<String> getAutori() {
+		return autori;
+	}
+	public void setAutori(List<String> autori) {
+		this.autori = autori;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	
+	
 }
